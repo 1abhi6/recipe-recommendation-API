ead1a07856f540a3b4efa944c3aff9e81fcd03b8
Abhishek Gupta <aiwithabhishek@gmail.com>
2025-09-24 11:47:40 +0530

    Some prodcution changes

--- src/autogen_agents_logic/model_client/model_client.py
+++ src/autogen_agents_logic/model_client/model_client.py
@@ -8,9 +8,9 @@ load_dotenv()
 
 class ModelClient:
     def __init__(self):
-        self.anthropic_model_client = AnthropicChatCompletionClient(
-            model="claude-opus-4-20250514", api_key=os.getenv("ANTHROPIC_API_KEY")
-        )
+        # self.anthropic_model_client = AnthropicChatCompletionClient(
+        #     model="claude-opus-4-20250514", api_key=os.getenv("ANTHROPIC_API_KEY")
+        # )
         
         self.model_client = OpenAIChatCompletionClient(
             model="gpt-4o", api_key=os.getenv("OPENAI_API_KEY")
